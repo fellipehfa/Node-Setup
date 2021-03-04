bd59b351750a11f193e5f2f3be34e321d7d06d6f
fellipehfa <fellipehf@gmail.com>
2021-03-04 17:11:41 -0300

    standard style

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+  return response.json({ message: 'Hello World' });
+});
 
-app.listen(3535, () => console.log("Server's running!⚡⚡⚡"))
+app.listen(3535, () => console.log("Server's running!⚡⚡⚡"));
